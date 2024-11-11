5e4a36749b57bd028e991209c284d5f4df9be37c
Ashlee <ashleedeboer@gmail.com>
2024-11-12 08:25:28 +1030

    updated db code

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "1T1I1m1e")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 
-DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+# DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 
 # FOR HEROKU
 # Get the DATABASE_URL from the environment and replace 'postgres://' with 'postgresql://'
-# DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
